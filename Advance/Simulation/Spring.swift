55f815beab99fac805802e3d54a02f169d82b61a
Timothy Donnelly <tim@tdonnelly.com>
2018-02-17 16:39:43 -0800

    Broke Spring down into Simulator type

--- Advance/Simulation/Spring.swift
+++ Advance/Simulation/Spring.swift
@@ -18,11 +18,11 @@
 /// s.target = CGPoint(x: 100.0, y: 200.0)
 /// // Off it goes!
 /// ```
-public final class Spring<Value: VectorConvertible> {
+public class Simulator<T> where T: SimulationFunction {
     
-    private let changedSink = Sink<Value>()
+    private let changedSink = Sink<T.Result>()
     
-    fileprivate var solver: Simulation<SpringFunction<Value>> {
+    fileprivate var solver: Simulation<T> {
         didSet {
             lastNotifiedValue = solver.value
             if solver.settled == false && subscription.paused == true {
@@ -45,11 +45,11 @@ public final class Spring<Value: VectorConvertible> {
     }()
     
     /// Fires when `value` has changed.
-    public var changed: Observable<Value> {
+    public var changed: Observable<T.Result> {
         return changedSink.observable
     }
     
-    fileprivate var lastNotifiedValue: Value {
+    fileprivate var lastNotifiedValue: T.Result {
         didSet {
             guard lastNotifiedValue != oldValue else { return }
             changedSink.send(value: lastNotifiedValue)
@@ -61,35 +61,45 @@ public final class Spring<Value: VectorConvertible> {
     /// - parameter value: The initial value of the spring. The spring will be
     ///   initialized with `target` and `value` equal to the given value, and
     ///   a velocity of `0`.
-    public init(value: Value) {
-        let f = SpringFunction(target: value)
-        solver = Simulation(function: f, value: value)
+    public init(function: T, value: T.Result) {
+        solver = Simulation(function: function, value: value)
         lastNotifiedValue = value
     }
     
+
+    
+    /// The current value of the spring.
+    public var value: T.Result {
+        get { return solver.value }
+        set { solver.value = newValue }
+    }
+    
+    /// The current velocity of the simulation.
+    public var velocity: T.Result {
+        get { return solver.velocity }
+        set { solver.velocity = newValue }
+    }
+
+}
+
+public final class Spring<T>: Simulator<SpringFunction<T>> where T: VectorConvertible {
+    
+    public init(value: T) {
+        let spring = SpringFunction(target: value)
+        super.init(function: spring, value: value)
+    }
+    
     /// Removes any current velocity and snaps the spring directly to the given value.
-    public func reset(to value: Value) {
+    public func reset(to value: T) {
         var f = solver.function
         f.target = value
         solver = Simulation(function: f, value: value)
         lastNotifiedValue = value
     }
     
-    /// The current value of the spring.
-    public var value: Value {
-        get { return solver.value }
-        set { solver.value = newValue }
-    }
-    
-    /// The current velocity of the simulation.
-    public var velocity: Value {
-        get { return solver.velocity }
-        set { solver.velocity = newValue }
-    }
-    
-    /// The target value of the spring. As the simulation runs, `value` will be 
+    /// The target value of the spring. As the simulation runs, `value` will be
     /// pulled toward this value.
-    public var target: Value {
+    public var target: T {
         get { return solver.function.target }
         set { solver.function.target = newValue }
     }
@@ -99,4 +109,5 @@ public final class Spring<Value: VectorConvertible> {
         get { return solver.function.configuration }
         set { solver.function.configuration = newValue }
     }
+    
 }
