0fdf9e05995f0b51f1b2d47a49986319855782a4
Timothy Donnelly <tim@tdonnelly.com>
2018-09-24 22:13:06 -0700

    Updated documentation generation

--- rebuild_docs.sh
+++ rebuild_docs.sh
@@ -1,6 +1,21 @@
-jazzy \
-  --clean \
-  --source-directory ./\
-  --output docs
+#!/bin/sh
 
-open "docs/index.html"
+rm -rf ./Docs
+
+cd SampleApp
+
+bundle install
+
+bundle exec pod install
+
+jazzy \
+  --config ../.jazzy.yaml \
+  --xcodebuild-arguments -workspace,SampleApp.xcworkspace,-scheme,Advance-iOS \
+  --clean \
+  --readme ../README.md \
+  --output ../Docs
+
+
+cd ..
+
+open "Docs/index.html"
