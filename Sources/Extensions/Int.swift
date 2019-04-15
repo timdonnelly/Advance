51eb3517f035c0406d1fee88e7e339ce8c4ab183
Timothy Donnelly <tim@tdonnelly.com>
2019-04-15 12:10:57 -0700

    Remove more API from Vector

--- Sources/Extensions/Int.swift
+++
@@ -1,8 +0,0 @@
-extension Int: Interpolatable {
-    
-    public func interpolated(to otherValue: Int, alpha: Double) -> Int {
-        let result = Double(self).interpolated(to: Double(otherValue), alpha: alpha)
-        return Int(result)
-    }
-    
-}
