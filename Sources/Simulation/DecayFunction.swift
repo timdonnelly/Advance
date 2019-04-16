e6d8b1c82675d15441bf2fe646c9b7ab791f811d
Timothy Donnelly <tim@tdonnelly.com>
2019-04-16 10:57:29 -0700

    Public API cleanup

--- Sources/Simulation/DecayFunction.swift
+++ Sources/Simulation/DecayFunction.swift
@@ -3,13 +3,16 @@ public struct DecayFunction<T>: SimulationFunction where T: SIMD, T.Scalar == Do
     
     /// How close to 0 each component of the velocity must be before the
     /// simulation is allowed to converge.
-    public var threshold: Double = 0.1
+    public var threshold: Double
     
     /// How much to erode the velocity.
-    public var drag: Double = 3.0
+    public var drag: Double
     
     /// Creates a new `DecayFunction` instance.
-    public init() {}
+    public init(threshold: Double = 0.1, drag: Double = 3.0) {
+        self.threshold = threshold
+        self.drag = drag
+    }
     
     /// Calculates acceleration for a given state of the simulation.
     public func acceleration(value: T, velocity: T) -> T {
--- Sources/Simulation/SimulationFunction.swift
+++ Sources/Simulation/SimulationFunction.swift
@@ -1,6 +1,5 @@
 /// Conforming types implement a dynamic function that models changes to
 /// a vector over time.
-
 public protocol SimulationFunction {
     
     /// The type of vector driven by the simulation
@@ -29,47 +28,4 @@ public enum Convergence<T> where T: SIMD, T.Scalar == Double {
     
     /// The simulation should converge (come to rest) with the given value.
     case converge(atValue: T)
-}
-
-
-extension SimulationFunction {
-    
-    private typealias Derivative = (value: VectorType, velocity: VectorType)
-    
-    /// Integrates time into an existing simulation state, returning the resulting
-    /// simulation state.
-    ///
-    /// The integration is done via RK4.
-    public func integrate(value: VectorType, velocity: VectorType, time: Double) -> (value: VectorType, velocity: VectorType) {
-        
-        let initial = Derivative(value: .zero, velocity: .zero)
-        
-        let a = evaluate(value: value, velocity: velocity, time: 0.0, derivative: initial)
-        let b = evaluate(value: value, velocity: velocity, time: time * 0.5, derivative: a)
-        let c = evaluate(value: value, velocity: velocity, time: time * 0.5, derivative: b)
-        let d = evaluate(value: value, velocity: velocity, time: time, derivative: c)
-        
-        var dxdt = a.value
-        dxdt += (2.0 * (b.value + c.value)) + d.value
-        dxdt = Double(1.0/6.0) * dxdt
-        
-        var dvdt = a.velocity
-        dvdt += (2.0 * (b.velocity + c.velocity)) + d.velocity
-        dvdt = Double(1.0/6.0) * dvdt
-        
-        return (
-            value: value + (time * dxdt),
-            velocity: velocity + (time * dvdt)
-        )
-        
-    }
-    
-    private func evaluate(value: VectorType, velocity: VectorType, time: Double, derivative: Derivative) -> Derivative {
-        let nextValue = value + (time * derivative.value)
-        let nextVelocity = velocity + (time * derivative.velocity)
-        return Derivative(
-            value: nextVelocity,
-            velocity: acceleration(value: nextValue, velocity: nextVelocity))
-    }
-    
 }
