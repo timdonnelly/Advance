45716226e3de570350b469c0d5d88ecfa3c0f51b
Timothy Donnelly <tim@tdonnelly.com>
2019-04-19 10:50:31 -0700

    Remove playground

--- Playground.playground/Pages/Spring.xcplaygroundpage/Contents.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-import PlaygroundSupport
-import Advance
-
-let animation = 0.0.springAnimation(to: 100.0, initialVelocity: 0.0, tension: 20.0, damping: 2.0, threshold: 0.1)
-
-animation.allValues().map { $0 }
