1d71c3184a5a787ac960a588662e34bf8d4f08b6
Timothy Donnelly <tim@tdonnelly.com>
2019-04-19 09:51:23 -0700

    Spring is now powered by Animator

--- Sources/Spring.swift
+++ Sources/Spring.swift
@@ -7,74 +7,85 @@
 /// ```
 public final class Spring<Value: VectorConvertible> {
     
-    private let displayLink = DisplayLink()
+    private let animator: Animator<Value>
     
-    private var function: SpringFunction<Value> {
-        didSet {
-            simulation.use(function: function)
-        }
-    }
-    
-    private var simulation: Simulation<Value> {
-        didSet {
-            displayLink.isPaused = simulation.hasConverged
-            if simulation.value != oldValue.value {
-                onChange?(simulation.value)
-            }
-        }
-    }
+    private var function: SpringFunction<Value>
     
     /// Initializes a new spring converged at the given value, using default configuration options for the spring function.
     public init(initialValue: Value) {
         function = SpringFunction(target: initialValue)
-        simulation = Simulation(function: function, initialValue: initialValue)
-        displayLink.onFrame = { [weak self] frame in
-            self?.simulation.advance(by: frame.duration)
+        animator = Animator(initialValue: initialValue)
+    }
+    
+    public var onChange: ((Value) -> Void)? {
+        get { return animator.onChange }
+        set { animator.onChange = newValue }
+    }
+    
+    public var value: Value {
+        get { return animator.value }
+        set {
+            let velocity = animator.velocity
+            animator.value = newValue
+            applyFunction(impartingVelocity: velocity)
         }
     }
     
-    public var onChange: ((Value) -> Void)?
-    
-    public var value: Value {
-        get { return simulation.value }
-        set { simulation.value = newValue }
-    }
-    
     public var velocity: Value {
-        get { return simulation.velocity }
-        set { simulation.velocity = newValue }
+        get { return animator.velocity }
+        set { applyFunction(impartingVelocity: newValue) }
     }
     
     /// The spring's target.
     public var target: Value {
         get { return function.target }
-        set { function.target = newValue }
+        set {
+            function.target = newValue
+            applyFunction()
+        }
     }
     
     /// Removes any current velocity and snaps the spring directly to the given value.
     /// - Parameter value: The new value that the spring will be reset to.
     public func reset(to value: Value) {
         function.target = value
-        simulation.value = value
+        animator.value = value
     }
     
     /// How strongly the spring will pull the value toward the target,
     public var tension: Double {
         get { return function.tension }
-        set { function.tension = newValue }
+        set {
+            function.tension = newValue
+            applyFunction()
+        }
     }
     
     /// The resistance that the spring encounters while moving the value.
     public var damping: Double {
         get { return function.damping }
-        set { function.damping = newValue }
+        set {
+            function.damping = newValue
+            applyFunction()
+        }
     }
     
     /// The minimum distance from the target value (for each component) that the
     /// current value can be in order to enter a converged (settled) state.
     public var threshold: Double {
         get { return function.threshold }
-        set { function.threshold = newValue }
+        set {
+            function.threshold = newValue
+            applyFunction()
+        }
+    }
+    
+    private func applyFunction(impartingVelocity velocity: Value? = nil) {
+        if let velocity = velocity {
+            animator.simulate(using: function, initialVelocity: velocity)
+        } else {
+            animator.simulate(using: function)
+        }
     }
     
 }
