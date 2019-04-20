06f6084c9ef33c8634e92012595dbf4bd1b97af5
Timothy Donnelly <tim@tdonnelly.com>
2019-04-19 21:11:24 -0700

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Advance",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "Advance",
+            targets: ["Advance"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Advance",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "AdvanceTests",
+            dependencies: ["Advance"],
+            path: "Tests/Sources"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
