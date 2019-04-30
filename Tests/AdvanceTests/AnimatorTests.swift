3c8e861890738bfbe711b3485392c6112c1fae13
Timothy Donnelly <tim@tdonnelly.com>
2019-04-30 12:56:48 -0700

    Add completion arguments to Animator

--- Tests/AdvanceTests/AnimatorTests.swift
+++ Tests/AdvanceTests/AnimatorTests.swift
@@ -54,6 +54,66 @@ class AnimatorTests: XCTestCase {
         
     }
     
+    func testCancelRunningAnimationCallsCompletion() {
+        
+        let finishedExpectation = expectation(description: "The animator called completion")
+
+        
+        let animator = Animator(initialValue: 0.0)
+        let toValue = 10.0
+        animator.animate(to: toValue, duration: 1.0, completion: { reason in
+            if reason == .interrupted {
+                finishedExpectation.fulfill()
+            }
+        })
+        
+        animator.onChange = { [weak animator] value in
+            guard value > toValue/2.0 else { return }
+            animator?.cancelRunningAnimation()
+        }
+        
+        wait(for: [finishedExpectation], timeout: 2.0)
+        
+    }
+    
+    func testAnimationCallsCompletion() {
+        
+        let finishedExpectation = expectation(description: "The animator called completion")
+        
+        
+        let animator = Animator(initialValue: 0.0)
+        let toValue = 10.0
+        animator.animate(to: toValue, duration: 1.0, completion: { reason in
+            if reason == .finished {
+                finishedExpectation.fulfill()
+            }
+        })
+        
+        wait(for: [finishedExpectation], timeout: 2.0)
+        
+    }
+    
+    func testSimulationCallsCompletion() {
+        
+        let finishedExpectation = expectation(description: "The animator called completion")
+        
+        
+        let animator = Animator(initialValue: 0.0)
+        let spring = SpringFunction(target: 10.0, tension: 300, damping: 30, threshold: 0.1)
+        animator.simulate(
+            using: spring,
+            completion: { reason in
+                if reason == .finished {
+                    finishedExpectation.fulfill()
+                }
+            })
+        
+        let estimatedDuration = spring.estimatedConvergence(initialValue: 0.0, initialVelocity: 0.0, maximumDuration: 20.0)!.duration
+        
+        wait(for: [finishedExpectation], timeout: estimatedDuration)
+        
+    }
+    
     static var allTests = [
         ("testTimedAnimationsFinishOnTargetValue", testTimedAnimationsFinishOnTargetValue),
         ("testCancelRunningAnimationRemainsOnCurrentValue", testCancelRunningAnimationRemainsOnCurrentValue)
